3200352d7236a6da7345634bebd3c59a0eaba77a
Diego Augusto <diego@diego-IPMH61R2.(none)>
2013-07-05 22:41:36 -0300

    Implementação exata do exemplo

--- js/stack.js
+++ js/stack.js
@@ -42,7 +42,7 @@ var svg = d3.select("#container-grafico").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
+d3.tsv("dados/data.tsv", function(error, data) {
   color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
   data.forEach(function(d) {
